3762b77d4ccdabb5b24cb70c69fc5fbff956589e
Matt Walsh <github@netbymatt.com>
2021-09-29 15:36:04 -0500

    remove gulp-bump

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,4 @@
 const gulp = require('gulp');
-const bump = require('gulp-bump');
 
 gulp.task('update-vendor', require('./gulp/update-vendor'));
 gulp.task('publish-frontend', require('./gulp/publish-frontend'));
-
-gulp.task('bump_patch', () => gulp.src('./package.json')
-	.pipe(bump())
-	.pipe(gulp.dest('./')));
-
-gulp.task('bump_minor', () => gulp.src('./package.json')
-	.pipe(bump({ type: 'minor' }))
-	.pipe(gulp.dest('./')));
-
-gulp.task('bump_major', () => gulp.src('./package.json')
-	.pipe(bump({ type: 'major' }))
-	.pipe(gulp.dest('./')));
