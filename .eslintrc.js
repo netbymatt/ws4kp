89d722d31388034b1d5c6cef1ad16161e6025982
Matt Walsh <github@netbymatt.com>
2022-03-01 16:04:20 -0600

    clean up eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,27 +22,6 @@ module.exports = {
 		'no-tabs': 0,
 		'no-console': 0,
 		'max-len': 0,
-		'linebreak-style': 0,
-		quotes: [
-			'error',
-			'single',
-		],
-		semi: [
-			'error',
-			'always',
-		],
-		'no-prototype-builtins': 0,
-		'comma-dangle': ['error', 'always-multiline'],
-		'block-scoped-var': ['error'],
-		'default-case': ['error'],
-		'default-param-last': ['error'],
-		'dot-location': ['error', 'property'],
-		eqeqeq: ['error'],
-		'no-eval': ['error'],
-		'no-eq-null': ['error'],
-		'no-floating-decimal': ['error'],
-		'no-trailing-spaces': ['error'],
-		'brace-style': [2, '1tbs', { allowSingleLine: true }],
 		'no-mixed-operators': [
 			'error',
 			{
