49296e53f0b516eed3b8bc3d454477432a9ecc6a
Matt Walsh <github@netbymatt.com>
2022-12-14 11:22:55 -0600

    make gulp output easier to read

--- gulp/publish-frontend.js
+++ gulp/publish-frontend.js
@@ -164,4 +164,6 @@ gulp.task('invalidate', async () => cloudfront.createInvalidation({
 	},
 }).promise());
 
-module.exports = gulp.series(clean, gulp.parallel('build_js', 'compress_js_data', 'compress_js_vendor', 'copy_css', 'compress_html', 'copy_other_files'), gulp.parallel('upload', 'upload_images'), 'invalidate');
+// upload_images could be in parallel with upload, but _images logs a lot and has little changes
+// by running upload last the majority of the changes will be at the bottom of the log for easy viewing
+module.exports = gulp.series(clean, gulp.parallel('build_js', 'compress_js_data', 'compress_js_vendor', 'copy_css', 'compress_html', 'copy_other_files'), 'upload_images', 'upload', 'invalidate');
