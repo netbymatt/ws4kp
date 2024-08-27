019908684b2a2d11b21a8f563471a14a717f57ef
Matt Walsh <github@netbymatt.com>
2024-08-27 09:00:38 -0500

    update dependencies and clarify dev dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
 	},
 	extends: [
 		'airbnb-base',
-		'plugin:sonarjs/recommended',
 	],
 	globals: {
 		Atomics: 'readonly',
@@ -25,8 +24,6 @@ module.exports = {
 		ecmaVersion: 2023,
 	},
 	plugins: [
-		'unicorn',
-		'sonarjs',
 	],
 	rules: {
 		indent: [
@@ -67,24 +64,6 @@ module.exports = {
 				json: 'always',
 			},
 		],
-		// unicorn
-		'unicorn/numeric-separators-style': 'error',
-		'unicorn/prefer-query-selector': 'error',
-		'unicorn/catch-error-name': 'error',
-		'unicorn/no-negated-condition': 'error',
-		'unicorn/better-regex': 'error',
-		'unicorn/consistent-function-scoping': 'error',
-		'unicorn/prefer-array-flat-map': 'error',
-		'unicorn/prefer-array-find': 'error',
-		'unicorn/prefer-regexp-test': 'error',
-		'unicorn/consistent-destructuring': 'error',
-		'unicorn/prefer-date-now': 'error',
-		'unicorn/prefer-ternary': 'error',
-		'unicorn/prefer-dom-node-append': 'error',
-		'unicorn/explicit-length-check': 'error',
-		'unicorn/prefer-at': 'error',
-		// sonarjs
-		'sonarjs/cognitive-complexity': 0,
 	},
 	ignorePatterns: [
 		'*.min.js',
--- server/scripts/modules/icons.mjs
+++ server/scripts/modules/icons.mjs
@@ -1,4 +1,3 @@
-/* eslint-disable unicorn/consistent-function-scoping */
 /* spell-checker: disable */
 
 const getWeatherRegionalIconFromIconLink = (link, _isNightTime) => {
