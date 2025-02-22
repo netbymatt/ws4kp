8253bb6e15bd3e5c225c4419bae88b3edfadb284
mwood77 <43637076+mwood77@users.noreply.github.com>
2025-02-22 17:09:33 +0100

    rewrite `extendedforecast.mjs` for open-meteo data

--- server/scripts/modules/extendedforecast.mjs
+++ server/scripts/modules/extendedforecast.mjs
@@ -2,12 +2,10 @@
 // technically uses the same data as the local forecast, we'll let the browser do the caching of that
 
 import STATUS from './status.mjs';
-import { json } from './utils/fetch.mjs';
-import { DateTime } from '../vendor/auto/luxon.mjs';
 import { getWeatherIconFromIconLink } from './icons.mjs';
-import { preloadImg } from './utils/image.mjs';
 import WeatherDisplay from './weatherdisplay.mjs';
 import { registerDisplay } from './navigation.mjs';
+import { getConditionText } from './utils/weather.mjs';
 
 class ExtendedForecast extends WeatherDisplay {
 	constructor(navId, elemId) {
@@ -19,26 +17,8 @@ class ExtendedForecast extends WeatherDisplay {
 
 	async getData(_weatherParameters) {
 		if (!super.getData(_weatherParameters)) return;
-		const weatherParameters = _weatherParameters ?? this.weatherParameters;
 
-		// request us or si units
-		let forecast;
-		try {
-			forecast = await json(weatherParameters.forecast, {
-				data: {
-					units: 'us',
-				},
-				retryCount: 3,
-				stillWaiting: () => this.stillWaiting(),
-			});
-		} catch (error) {
-			console.error('Unable to get extended forecast');
-			console.error(error.status, error.responseJSON);
-			this.setStatus(STATUS.failed);
-			return;
-		}
-		// we only get here if there was no error above
-		this.data = parse(forecast.properties.periods);
+		this.data = parse(_weatherParameters);
 		this.screenIndex = 0;
 		this.setStatus(STATUS.loaded);
 	}
@@ -79,86 +59,25 @@ class ExtendedForecast extends WeatherDisplay {
 
 // the api provides the forecast in 12 hour increments, flatten to day increments with high and low temperatures
 const parse = (fullForecast) => {
-	// create a list of days starting with today
-	const Days = [0, 1, 2, 3, 4, 5, 6];
-
-	const dates = Days.map((shift) => {
-		const date = DateTime.local().startOf('day').plus({ days: shift });
-		return date.toLocaleString({ weekday: 'short' });
-	});
-
-	// track the destination forecast index
-	let destIndex = 0;
 	const forecast = [];
-	fullForecast.forEach((period) => {
-		// create the destination object if necessary
-		if (!forecast[destIndex]) {
-			forecast.push({
-				dayName: '', low: undefined, high: undefined, text: undefined, icon: undefined,
-			});
-		}
-		// get the object to modify/populate
-		const fDay = forecast[destIndex];
-		// high temperature will always be last in the source array so it will overwrite the low values assigned below
-		fDay.icon = getWeatherIconFromIconLink(period.icon);
-		fDay.text = shortenExtendedForecastText(period.shortForecast);
-		fDay.dayName = dates[destIndex];
 
-		// preload the icon
-		preloadImg(fDay.icon);
+	Object.values(fullForecast.forecast).forEach((period) => {
+		const text = getConditionText(parseInt(period.weather_code, 10));
+		const date = new Date(period.hours[0].time);
 
-		if (period.isDaytime) {
-			// day time is the high temperature
-			fDay.high = period.temperature;
-			destIndex += 1;
-		} else {
-			// low temperature
-			fDay.low = period.temperature;
-		}
+		const fDay = {
+			text,
+			icon: getWeatherIconFromIconLink(text, fullForecast.timeZone),
+			date: period.hours[0].time,
+			dayName: date.toLocaleDateString('en-US', { weekday: 'long' }),
+			high: period.temperature_2m_max,
+			low: period.temperature_2m_min,
+		};
+
+		forecast.push(fDay);
 	});
 
 	return forecast;
-};
-
-const shortenExtendedForecastText = (long) => {
-	const regexList = [
-		[/ and /gi, ' '],
-		[/slight /gi, ''],
-		[/chance /gi, ''],
-		[/very /gi, ''],
-		[/patchy /gi, ''],
-		[/areas /gi, ''],
-		[/dense /gi, ''],
-		[/Thunderstorm/g, 'T\'Storm'],
-	];
-		// run all regexes
-	const short = regexList.reduce((working, [regex, replace]) => working.replace(regex, replace), long);
-
-	let conditions = short.split(' ');
-	if (short.indexOf('then') !== -1) {
-		conditions = short.split(' then ');
-		conditions = conditions[1].split(' ');
-	}
-
-	let short1 = conditions[0].substr(0, 10);
-	let short2 = '';
-	if (conditions[1]) {
-		if (short1.endsWith('.')) {
-			short1 = short1.replace(/\./, '');
-		} else {
-			short2 = conditions[1].substr(0, 10);
-		}
-
-		if (short2 === 'Blowing') {
-			short2 = '';
-		}
-	}
-	let result = short1;
-	if (short2 !== '') {
-		result += ` ${short2}`;
-	}
-
-	return result;
 };
 
 // register display
